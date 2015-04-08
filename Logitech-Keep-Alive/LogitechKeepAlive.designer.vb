b766f1b1c87a7880d7d50c012c2aabc80dbe21a4
aktai0 <xtoken.x@gmail.com>
2015-04-08 06:09:20 -0700

    Initial commit of the project files. Hope this gitignore works

---
+++ Logitech-Keep-Alive/LogitechKeepAlive.designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PlaceholderForm
+   Inherits System.Windows.Forms.Form
+
+   'Form overrides dispose to clean up the component list.
+   <System.Diagnostics.DebuggerNonUserCode()> _
+   Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+      Try
+         If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+         End If
+      Finally
+         MyBase.Dispose(disposing)
+      End Try
+   End Sub
+
+   'Required by the Windows Form Designer
+   Private components As System.ComponentModel.IContainer
+
+   'NOTE: The following procedure is required by the Windows Form Designer
+   'It can be modified using the Windows Form Designer.  
+   'Do not modify it using the code editor.
+   <System.Diagnostics.DebuggerStepThrough()> _
+   Private Sub InitializeComponent()
+      Me.components = New System.ComponentModel.Container()
+      Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PlaceholderForm))
+      Me.LogitechKeepAliveIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+      Me.AudioTimer = New System.Windows.Forms.Timer(Me.components)
+      Me.IconContextMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+      Me.EnabledToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+      Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+      Me.IconContextMenu.SuspendLayout()
+      Me.SuspendLayout()
+      '
+      'LogitechKeepAliveIcon
+      '
+      Me.LogitechKeepAliveIcon.Icon = CType(resources.GetObject("LogitechKeepAliveIcon.Icon"), System.Drawing.Icon)
+      Me.LogitechKeepAliveIcon.Text = "Logitech Headset Keep-Alive"
+      Me.LogitechKeepAliveIcon.Visible = True
+      '
+      'AudioTimer
+      '
+      Me.AudioTimer.Enabled = True
+      Me.AudioTimer.Interval = 300000
+      '
+      'IconContextMenu
+      '
+      Me.IconContextMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EnabledToolStripMenuItem, Me.ExitToolStripMenuItem})
+      Me.IconContextMenu.Name = "IconContextMenu"
+      Me.IconContextMenu.Size = New System.Drawing.Size(133, 52)
+      Me.IconContextMenu.Text = "asdf"
+      '
+      'EnabledToolStripMenuItem
+      '
+      Me.EnabledToolStripMenuItem.Checked = True
+      Me.EnabledToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
+      Me.EnabledToolStripMenuItem.Name = "EnabledToolStripMenuItem"
+      Me.EnabledToolStripMenuItem.Size = New System.Drawing.Size(132, 24)
+      Me.EnabledToolStripMenuItem.Text = "Enabled"
+      '
+      'ExitToolStripMenuItem
+      '
+      Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+      Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(132, 24)
+      Me.ExitToolStripMenuItem.Text = "Exit"
+      '
+      'PlaceholderForm
+      '
+      Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+      Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+      Me.ClientSize = New System.Drawing.Size(501, 493)
+      Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+      Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+      Me.MaximizeBox = False
+      Me.MinimizeBox = False
+      Me.Name = "PlaceholderForm"
+      Me.ShowInTaskbar = False
+      Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+      Me.Text = "Logitech Keep-Alive"
+      Me.TopMost = True
+      Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+      Me.IconContextMenu.ResumeLayout(False)
+      Me.ResumeLayout(False)
+
+   End Sub
+   Friend WithEvents LogitechKeepAliveIcon As System.Windows.Forms.NotifyIcon
+   Friend WithEvents AudioTimer As System.Windows.Forms.Timer
+   Friend WithEvents IconContextMenu As System.Windows.Forms.ContextMenuStrip
+   Friend WithEvents EnabledToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+   Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Logitech-Keep-Alive/LogitechKeepAlive.vb
@@ -0,0 +1,65 @@
+﻿Imports Microsoft.VisualBasic.Devices.Audio
+
+Public Class PlaceholderForm
+   Shared SOUND_FILE As String = "Resources\nothing.wav"
+
+   Private Sub ReactivateHeadset()
+      Dim player As NAudio.Wave.WaveOut
+      Dim reader As NAudio.Wave.AudioFileReader
+
+      For i = 0 To NAudio.Wave.WaveOut.DeviceCount - 1
+         'Console.WriteLine("Reactivating " & i & " at " & DateTime.Now.ToString("hh:mm:ss"))
+         player = New NAudio.Wave.WaveOut()
+         reader = New NAudio.Wave.AudioFileReader(SOUND_FILE)
+
+         With player
+            .DeviceNumber = i
+            .Init(reader)
+            .Play()
+         End With
+
+         Threading.Thread.Sleep(100)
+
+         player.Stop()
+
+         reader.Dispose()
+         player.Dispose()
+      Next
+   End Sub
+
+   Private Sub LogitechKeepAliveIcon_MouseClick(sender As Object, e As MouseEventArgs) Handles LogitechKeepAliveIcon.MouseClick
+      If e.Button = Windows.Forms.MouseButtons.Right Then
+         Me.Show()
+         Me.Activate()
+         Me.Width = 1
+         Me.Height = 1
+         IconContextMenu.Show(Cursor.Position)
+         Me.Left = IconContextMenu.Left + 1
+         Me.Top = IconContextMenu.Top + 1
+      End If
+   End Sub
+
+   Private Sub PlaceholderForm_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+      Me.Hide()
+   End Sub
+
+   Private Sub EnabledToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EnabledToolStripMenuItem.Click
+      EnabledToolStripMenuItem.Checked = Not EnabledToolStripMenuItem.Checked
+      AudioTimer.Enabled = EnabledToolStripMenuItem.Checked
+      If EnabledToolStripMenuItem.Checked Then
+         ReactivateHeadset()
+      End If
+   End Sub
+
+   Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+      Me.Close()
+   End Sub
+
+   Private Sub AudioTimer_Tick(sender As Object, e As EventArgs) Handles AudioTimer.Tick
+      ReactivateHeadset()
+   End Sub
+
+   Private Sub PlaceholderForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      ReactivateHeadset()
+   End Sub
+End Class
